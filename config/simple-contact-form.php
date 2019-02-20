9a45c8dfd89bf3071f4931ce0cbd124b31b25dd4
Mike Wall <daikazu@gmail.com>
2019-02-20 15:15:57 -0500

    added ability to toggle storage and sendign of email

--- config/simple-contact-form.php
+++ config/simple-contact-form.php
@@ -5,4 +5,10 @@ return [
     'from_address'    => env('SIMPLE_CONTACT_FORM_FROM_ADDRESS'),
     'subject'         => env('SIMPLE_CONTACT_FORM_SUBJECT', 'Contact Form Request'),
     'success_message' => env('SIMPLE_CONTACT_SUCCESS_MESSAGE', 'Thanks for contacting us!'),
+
+    // Store the Request info in Database
+    'store_request'   => true,
+
+    // Send Email
+    'send_email'      => true,
 ];
--- src/Http/Controllers/SimpleContactFormController.php
+++ src/Http/Controllers/SimpleContactFormController.php
@@ -2,8 +2,8 @@
 
 namespace Daikazu\SimpleContactForm\Http\Controllers;
 
-use Mail;
 use Illuminate\Http\Request;
+use Mail;
 
 class SimpleContactFormController
 {
@@ -15,22 +15,28 @@ class SimpleContactFormController
             'message' => 'required',
         ]);
 
-        \Daikazu\SimpleContactForm\ContactUs::create([
-            'name'    => $request->get('name'),
-            'email'   => $request->get('email'),
-            'message' => $request->get('message'),
-            'meta'    => $request->ips(),
-        ]);
+        if (config('simple-contact-form.store_request')) {
 
-        Mail::send('simple-contact-form::email', [
-            'name'         => $request->get('name'),
-            'email'        => $request->get('email'),
-            'user_message' => $request->get('message'),
-        ], function ($message) {
-            $message->from(config('simple-contact-form.from_address'));
-            $message->to(config('simple-contact-form.to_address'));
-            $message->subject(config('simple-contact-form.subject'));
-        });
+            \Daikazu\SimpleContactForm\ContactUs::create([
+                'name'    => $request->get('name'),
+                'email'   => $request->get('email'),
+                'message' => $request->get('message'),
+                'meta'    => $request->ips(),
+            ]);
+        }
+
+        if (config('simple-contact-form.send_email')) {
+
+            Mail::send('simple-contact-form::email', [
+                'name'         => $request->get('name'),
+                'email'        => $request->get('email'),
+                'user_message' => $request->get('message'),
+            ], function ($message) {
+                $message->from(config('simple-contact-form.from_address'));
+                $message->to(config('simple-contact-form.to_address'));
+                $message->subject(config('simple-contact-form.subject'));
+            });
+        }
 
         return back()->with('success', config('simple-contact-form.success_message'));
     }
